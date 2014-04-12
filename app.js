13b2186d87e6716aa91b51cf37566f1052ba0185
Skaryon <michael.klein.rulez@googlemail.com>
2014-04-12 10:11:56 +0200

    testcommit aus IDE2

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var erstiapp = (function() {
-    //public
-    var self = this;
     //private
+    var self = this;
+    var test = "hi";
+    //public
     return {
+       test2: "hallo",
        init: function() {
            $("#start").on("pageshow", function(event) {
                $('#start .openpanel').click(function() {
                    $('#mypanel').panel("open");
+                   alert(test);
+                   alert(this.test2)
                });
            });
        }
