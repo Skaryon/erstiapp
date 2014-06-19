1fb8b5911f9a78222d5247ff1793f84a7aa0be88
Skaryon <michael.klein.rulez@googlemail.com>
2014-06-19 13:07:31 +0200

    test.js

---
+++ test.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function test() {
+    
+}
+
+var test = (function() {
+    
+    var a = 5;
+    return {
+        b : function() {
+            
+        }
+    }
+})()
+
+test.b()
+test.a
+
+var test2 = {
+    a : "hallo",
+    b : "hallo2",
+    c: function() {
+        
+    }
+}
+
+alert(test2.c())
+
