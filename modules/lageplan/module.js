73cdbba2f8a0c0334eea38c0e105e702a20847a5
realraffi <realraffi@gmail.com>
2014-06-22 22:53:02 +0200

    Lageplan mit Openstreetmaps; Aufruf in eigenständiger Seite mit Leaflet-Plugin; Aufruf über iframe

--- modules/lageplan/module.js
+++ modules/lageplan/module.js
@@ -1,27 +1,29 @@
 var module;
-module = (function () {
+module = (function() {
     //private
     var self;
     var path;
     var $startPage;
     //public
     return {
-        init: function () {
+        init: function() {
             path = erstiapp.modules.registerModule("lageplan", this);
             erstiapp.menu.add(
-                "lageplan", {
-                    name: "Lageplan",
-                    $page: "#karte",
-                    icon: "",
-                    panel: true
-                }
+                    "lageplan", {
+                        name: "Lageplan",
+                        $page: "#karte",
+                        icon: "",
+                        panel: true
+                    }
             );
-            erstiapp.modules.loadPage('modules/' + path + '/karte.html', function ($page) {
+            erstiapp.modules.loadPage('modules/' + path + '/karte.html', function($page) {
                 $startPage = $page;
-                //$('iframe').iframeAutoHeight();
                 erstiapp.modules.finishedLoading("lageplan");
+                $startPage.on('pagebeforeshow', function(event) {
+                    $('#mapFrame').height($(window).height() - 130);
+                })
             });
         }
     }
 })
-().init();
+        ().init();
