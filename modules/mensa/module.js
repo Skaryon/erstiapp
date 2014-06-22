4d35fe7a7997c73b24874d155e76fd476b351864
Michael <michaelklein@Macintosh.local>
2014-06-22 11:07:44 +0200

    Mensa + Fragen

--- modules/mensa/module.js
+++ modules/mensa/module.js
@@ -18,6 +18,21 @@ module = (function() {
             );
             erstiapp.modules.loadPage('modules/' + path + '/mensa.html', function($page) {
                 $mensaPage = $page;
+                $mensaPage.on("pagebeforeshow", function(event) {
+                    $.ajax({
+                        type: 'GET',
+                        async: false,
+                        dataType: "html",
+                        url: erstiapp.getServer() + "getMensa",
+                        complete: function(responseData, textStatus, jqXHR) {
+                            if (textStatus != "error") {
+                                $mensaPage.find("div[data-role='content']").html(responseData['responseText']);
+                            }
+                        },
+                        error: function(responseData, textStatus, errorThrown) {
+                        }
+                    });
+                });
                 erstiapp.modules.finishedLoading("mensa");
             });
         }
--- modules/q_and_i/module.js
+++ modules/q_and_i/module.js
@@ -1,28 +1,85 @@
 var module;
-module = (function () {
+module = (function() {
     //private
     var self;
     var path;
     var $startPage;
+    var $questionPage;
     //public
     return {
-        init: function () {
+        init: function() {
             path = erstiapp.modules.registerModule("qi", this, {
                 search: true
             });
             erstiapp.menu.add(
-                "qi", {
-                    name: "Fragen & Antworten",
-                    $page: "#qi_start",
-                    icon: "",
-                    panel: true
-                }
+                    "qi", {
+                        name: "Fragen & Antworten",
+                        $page: "#qi_start",
+                        icon: "",
+                        panel: true
+                    }
             );
-            erstiapp.modules.loadPage('modules/' + path + '/start.html', function ($page) {
-                $startPage = $page;
-                erstiapp.modules.finishedLoading("module1");
+            erstiapp.menu.add(
+                    "qi_question", {
+                        name: "Frage",
+                        $page: "#qi_question",
+                        icon: "",
+                        panel: true,
+                        hide: true
+                    }
+            );
+            erstiapp.modules.loadPage('modules/' + path + '/question.html', function($page) {
+                $questionPage = $page;
+                erstiapp.modules.loadPage('modules/' + path + '/start.html', function($page) {
+                    $startPage = $page;
+                    $startPage.on("pagebeforeshow", function(event) {
+                        $('#questionButton').click(function() {
+                            if ($("#qi_start #questionCat option:selected").val() === "0")
+                                erstiapp.exception("Bitte wähle eine Kategorie aus!", "#" + $startPage.attr("id"));
+                            else {
+                                console.log(erstiapp.getServer() + "askQuestion?question=" + $('#qi_start #question').val() + "&category=" + $("#qi_start #questionCat option:selected").val() + "&uID=" + erstiapp.login.getUserId() + "&uPass=" + erstiapp.login.getUserPass())
+                                $.ajax({
+                                    type: 'GET',
+                                    async: true,
+                                    dataType: "json",
+                                    url: erstiapp.getServer() + "askQuestion?question=" + $('#qi_start #question').val() + "&category=" + $("#qi_start #questionCat option:selected").val() + "&uID=" + erstiapp.login.getUserId() + "&uPass=" + erstiapp.login.getUserPass(),
+                                    complete: function(responseData, textStatus, jqXHR) {
+                                        if (textStatus != "error") {
+                                            var data = JSON.parse(responseData['responseText']);
+                                            console.log(data.result);
+                                            erstiapp.exception(data.error, "#" + $startPage.attr("id"));
+                                            if (data.result) {
+                                                console.log(data.id)
+                                                erstiapp.changePage("#qi_question", {}, {id: data.id});
+                                            }
+                                        }
+                                    },
+                                    error: function(responseData, textStatus, errorThrown) {
+                                    }
+                                });
+                            }
+                        });
+                    });
+                    $questionPage.on("pagebeforeshow", function(event) {
+                        $.ajax({
+                            type: 'GET',
+                            async: true,
+                            dataType: "json",
+                            url: erstiapp.getServer() + "getQuestion?id=" + erstiapp.getParams().id,
+                            complete: function(responseData, textStatus, jqXHR) {
+                                if (textStatus != "error") {
+                                    var data = JSON.parse(responseData['responseText']);
+                                    $questionPage.find("#questionText").html(data.text);
+                                }
+                            },
+                            error: function(responseData, textStatus, errorThrown) {
+                            }
+                        });
+                    });
+                    erstiapp.modules.finishedLoading("module1");
+                });
             });
         }
     }
 })
-().init();
+        ().init();
